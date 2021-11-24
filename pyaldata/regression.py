4d8376e25a272b8e862cc69a84e9c72e43e82dc1
Bence Bagi <bagi.bence94@gmail.com>
2021-11-24 11:40:17 +0000

    Add expand_field_in_time funtion (#123)

--- pyaldata/regression.py
+++ pyaldata/regression.py
@@ -1,6 +1,8 @@
 from . import utils
 from . import extract_signals
 
+import numpy as np
+
 
 def fit_regressor_model(trial_data, model, input_signal, output_signal, train_indices=None):
     """
@@ -90,3 +92,30 @@ def regress(trial_data, model, input_signal, output_signal, out_fieldname, train
         return trial_data, model
     else:
         return trial_data
+
+
+@utils.copy_td
+def expand_field_in_time(trial_data, field, out_fieldname=None):
+    """
+    Expand scalar field to a vector that can be used for prediction per time point.
+
+    Parameters
+    ----------
+    trial_data : pd.DataFrame
+        data in trial_data format
+    field : string
+        field to expand
+    out_fieldname : string, default {field}_ext
+        field to put the expanded values to
+
+    Returns
+    -------
+    trial_data with out_fieldname added
+    """
+    if out_fieldname is None:
+        out_fieldname = field + "_ext"
+        
+    trial_data[out_fieldname] = [trial[field] * np.ones(utils.get_trial_length(trial))
+                                 for (i, trial) in trial_data.iterrows()]
+
+    return trial_data
