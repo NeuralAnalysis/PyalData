fe69461b208f33210a4ae8ea8193abac9d325708
Bence Bagi <bagi.bence94@gmail.com>
2022-04-25 14:12:14 +0100

    style: update integrity_checks.all_integer to use .astype

--- pyaldata/integrity_checks.py
+++ pyaldata/integrity_checks.py
@@ -16,8 +16,9 @@ def trials_are_same_length(trial_data, ref_field=None):
     trial_lengths = [utils.get_trial_length(trial, ref_field=ref_field) for (_, trial) in trial_data.iterrows()]
     return len(set(trial_lengths)) == 1
 
+
 def all_integer(arr):
     """
     Check if all the values in arr are approximately integers
     """
-    return np.all(np.isclose(arr, np.array(arr, dtype=int)))
+    return np.all(np.isclose(arr, arr.astype(int)))
