e33db88c739916ae6da3f34d28a5f44433fe1ba6
Bence Bagi <bagi.bence94@gmail.com>
2024-04-17 10:23:54 +0100

    Configure isort and ruff to match

--- pyaldata/__init__.py
+++ pyaldata/__init__.py
@@ -3,22 +3,18 @@
 from .array_utils import split_array
 from .cmp import read_cmp, remove_cmp_formatting
 from .df_utils import concat_TDs, copy_fields, rename_fields
-from .dim_reduction import (
-    apply_dim_reduce_model,
-    dim_reduce,
-    fit_dim_reduce_model
-)
+from .dim_reduction import apply_dim_reduce_model, dim_reduce, fit_dim_reduce_model
 from .extract_signals import (
     concat_trials,
     get_sig_by_trial,
     get_signals,
     reverse_concat,
-    stack_time_average
+    stack_time_average,
 )
 from .firing_rates import (
     add_firing_rates,
     get_average_firing_rates,
-    remove_low_firing_neurons
+    remove_low_firing_neurons,
 )
 from .integrity_checks import all_integer, trials_are_same_length
 from .interval import (
@@ -28,7 +24,7 @@ from .interval import (
     slice_around_index,
     slice_around_point,
     slice_between_points,
-    slice_in_trial
+    slice_in_trial,
 )
 from .io import mat2dataframe
 from .movement_onset_and_peak import (
@@ -38,13 +34,13 @@ from .movement_onset_and_peak import (
     get_movement_onset,
     get_onset_idx,
     get_peak_speed,
-    get_peak_speed_idx
+    get_peak_speed_idx,
 )
 from .regression import (
     apply_regressor_model,
     expand_field_in_time,
     fit_regressor_model,
-    regress
+    regress,
 )
 from .signal_transformations import (
     center,
@@ -56,22 +52,17 @@ from .signal_transformations import (
     transform_signal,
     z_score,
     z_score_signal,
-    zero_normalize_signal
+    zero_normalize_signal,
 )
 from .signals import add_gradient, add_norm, add_speed, signal_dimensionality
-from .smoothing import (
-    hw_to_std,
-    norm_gauss_window,
-    smooth_data,
-    smooth_signals
-)
+from .smoothing import hw_to_std, norm_gauss_window, smooth_data, smooth_signals
 from .tools import (
     combine_time_bins,
     keep_common_trials,
     merge_signals,
     select_trials,
     subtract_cross_condition_mean,
-    trial_average
+    trial_average,
 )
 from .utils import (
     copy_td,
@@ -80,5 +71,5 @@ from .utils import (
     get_string_fields,
     get_time_varying_fields,
     get_trial_length,
-    remove_suffix
+    remove_suffix,
 )
